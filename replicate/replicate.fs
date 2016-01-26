ab84069bd4f6964209eb307a03c813580f25dcc5
Lewis Smeeton <lcs137@bham.ac.uk>
2016-01-26 21:06:53 +0000

    Added helper function for mating
    replicate/replicate.fs:
    Added `matingPairsFromPopulation` function, which returns a list of pairs of organisms, given a Population type and a list of indices for input

--- replicate/replicate.fs
+++ replicate/replicate.fs
@@ -12,3 +12,39 @@
     let mateBinarySplice phenomeFromGenome organism1 organism2 boolList = 
         let childGenome = (binarySplice organism1.genome organism2.genome boolList) 
         {genome = childGenome; phenome = phenomeFromGenome childGenome}
+
+    
+    let matingPairsFromPopulation (Population population) (matingPairsByIndex : (int * int) list) = 
+        // return a list of Organism tuples from population according to 
+        // the pairs defined in matingTuples
+        let matingPairsFromPopulation' (population : Organism list) (matingPairsByIndex : (int * int) list) =
+            let getOrganism' (pop : Organism list) index = 
+                try
+                    Some pop.[index]
+                with
+                    | :? System.ArgumentException -> None
+
+            let addPairsToList population accOrgList pairIndices =
+                match (getOrganism' population (fst pairIndices), getOrganism' population (snd pairIndices)) with
+                    |(Some o1, Some o2) -> (o1,o2)::accOrgList 
+                    |(_,_) -> accOrgList
+
+            let rec matchPairs (pop : Organism list) (acc : (Organism * Organism) list) (mpbi : (int * int) list) =
+                match mpbi with
+                    |[] -> acc
+                    |_ -> matchPairs pop (addPairsToList pop acc (List.head mpbi)) (List.tail mpbi) 
+
+            let accOrgList = []
+
+            matchPairs population accOrgList matingPairsByIndex
+        
+        match population with
+            |[] -> []
+            |_ -> matingPairsFromPopulation' population matingPairsByIndex
+
+
+
+
+
+
+            
