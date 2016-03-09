bd40c9041bf8a98997f734a16025eb810c37ca87
Lewis Smeeton <lcs137@bham.ac.uk>
2016-03-09 22:01:51 +0000

    Added utility functions to truncate and extend a list of arbitrary type

---
+++ dawkins.testing/test_utility.fs
@@ -0,0 +1,40 @@
+﻿namespace dawkins.testing.utility
+
+open dawkins
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type ``Given a List, lst, of length, len`` ()=
+    
+    let lst = [0..9] // a list of ten elements, the ints 0-9
+    let len = List.length lst
+    let padding = 0
+    
+
+    [<Test>] member x.
+     ``truncateList will return lst when desiredLength = len.`` () =
+            FsUnit.Assert.AreEqual(utility.truncateList len lst, lst)
+
+    [<Test>] member x.
+     ``padList will return lst when desiredLength = len.`` () =
+            FsUnit.Assert.AreEqual(utility.padList padding len lst, lst)
+
+    [<Test>] member x.
+     ``truncateList will return lst when desiredLength < len.`` () =
+            FsUnit.Assert.AreEqual(utility.truncateList (len + 1) lst, lst)
+
+    [<Test>] member x.
+     ``padList will return lst when desiredLength > len.`` () =
+            FsUnit.Assert.AreEqual(utility.padList padding (len - 1) lst, lst)
+
+    [<Test>] member x.
+     ``truncateList will return a shortened list when desiredLength < len.`` () =
+            FsUnit.Assert.AreEqual(utility.truncateList (len - 1) lst, [1..9])
+
+    [<Test>] member x.
+     ``padList will return an expanded list when desiredLength > len.`` () =
+            FsUnit.Assert.AreEqual(utility.padList padding (len + 1) lst, 0::[0..9])
+
+
+
--- utils/utility.fs
+++ utils/utility.fs
@@ -9,3 +9,14 @@
     // apply a function to the second part of the tuple, retaining the first part
     let mapSecond f (a,b) = a, (f b)
     // apply a rop function to the first part of the tuple. If the result is success, the success case is a new tuple 
+
+    let rec truncateList desiredLength lst =
+        match desiredLength - List.length lst with
+            |x when x < 0 -> truncateList desiredLength (List.tail lst)
+            |_ -> lst
+
+    let rec padList padding desiredLength lst = 
+        match desiredLength - List.length lst with
+            |x when x > 0 -> padList padding desiredLength (padding::lst)
+            |_ -> lst
+
