41b9ac8df3ec4aaec3b7132c6c64af989d059da4
Lewis Smeeton <lcs137@bham.ac.uk>
2016-04-09 18:54:22 +0100

    Added test for `partnerListElements' function

--- dawkins.testing/test_replicate.fs
+++ dawkins.testing/test_replicate.fs
@@ -47,3 +47,29 @@ type ``Given a Population and Mating Function`` ()=
     [<Test>] member x.
      ``a mating list containing multiple pairs of indices (including errors) returns an organism list containing multiple pairs``() =
             FsUnit.Assert.AreEqual(replicate.matingPairsFromPopulation p [(0,0);(0,1);(1,2)], [o1,o2;o1,o1])
+
+[<TestFixture>]
+type ``Given a generic list of elements`` ()=
+    
+    let evenInputLst = [1..10]
+    let oddInputLst = [1..9]
+
+    let evenOutputLst = List.rev [for i in [1..2..10] do yield (i,i+1)]
+    let oddOutputLst = List.rev [for i in [1..2..8] do yield (i,i+1)]
+    
+
+    [<Test>] member x.
+     ``partnerListElements returns an empty output list when given an empty input list .`` () =
+            FsUnit.Assert.AreEqual(replicate.partnerListElements [], [])
+
+    [<Test>] member x.
+     ``partnerListElements returns an empty output list when given an input list containing a single element .`` () =
+            FsUnit.Assert.AreEqual(replicate.partnerListElements [1], [])
+
+    [<Test>] member x.
+     ``partnerListElements returns an output list containing N / 2 pairs when given an input list containing an even N elements .`` () =
+            FsUnit.Assert.AreEqual(replicate.partnerListElements evenInputLst, evenOutputLst)
+
+    [<Test>] member x.
+     ``partnerListElements returns an output list containing (N-1) / 2 pairs when given an input list containing an odd N elements .`` () =
+            FsUnit.Assert.AreEqual(replicate.partnerListElements oddInputLst, oddOutputLst)
