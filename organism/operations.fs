7fa3a98621f52e83b0566af13cd29d5c7c208e90
Lewis Smeeton <lcs137@bham.ac.uk>
2016-04-09 11:08:30 +0100

    Added functions to convert `Float lists' to and from `Signal' types

--- organism/operations.fs
+++ organism/operations.fs
@@ -1,12 +1,68 @@
 ﻿namespace organism
 
+    module signaloperations = 
+        open utility
+        open organism.signaltypes
+        open organism.types
+
+        let signalGenomeFromFloatList floatList = 
+
+            if List.length floatList <= 2 then
+                (0.0,[NoDelta]) |> SignalGenome
+            else
+
+                let signalFromFloats lastElement thisElement = 
+                    match thisElement - lastElement with 
+                    | 0.0 -> NoDelta
+                    | _ -> thisElement - lastElement |> Delta
+
+                let rec signalGenomeFromFloatList' signalList lastElement remainingFloatList =
+                    match remainingFloatList with
+                    |[]     -> signalList
+                    |hd::tl -> let signal = signalFromFloats lastElement hd
+                               let updatedSignalList = signal::signalList
+                               signalGenomeFromFloatList' updatedSignalList hd tl
+
+                let lastElement = List.head floatList
+                let remainingFloatList = List.tail floatList
+                
+                (floatList.[0],
+                 signalGenomeFromFloatList' []  lastElement remainingFloatList
+                 |> List.rev)
+                 |> SignalGenome
+
+        let floatListFromSignalGenome genome = 
+
+            let floatFromSignal (lastElement, signal) =
+                match signal with
+                    |Delta d -> lastElement + d
+                    |NoDelta -> lastElement
+
+            let rec floatListFromSignalGenome' floatList lastElement remainingSignalList = 
+                match remainingSignalList with
+                    |[]     -> floatList
+                    |hd::tl -> let flt = floatFromSignal (lastElement, hd)
+                               let updatedFloatList = flt::floatList
+                               floatListFromSignalGenome' updatedFloatList flt tl
+            
+            match genome with
+                |FloatGenome _ -> []
+                |SignalGenome (firstSignal, signals) 
+                    -> floatListFromSignalGenome' [] firstSignal signals
+                       |> List.rev
+                    
+
+            
+
     module operations =
         open utility
         open organism.types
 
-        let identityPhenomeFromGenome (Genome genome) =
+        let identityPhenomeFromGenome (genome) =
             // identity mapping of the genome to the phenome
-            Phenome(genome) 
+            match genome with
+                |FloatGenome f -> FloatPhenome(f)
+                |SignalGenome (s1,s2) -> SignalPhenome(s1,s2) 
 
         let organismFromGenome costFunction phenomeFromGenome genome =
             // Shorthand function for building an organism from a genome
