4d826b2d41d7b1591670c0305647f0913f74699f
Lewis Smeeton <lcs137@bham.ac.uk>
2016-04-22 20:49:11 +0100

    Added an illegal container index case to InputLegality type.  Added two custom exceptions for handling illegal input lengths and container indices

--- utils/inputlegallity.fs
+++ utils/inputlegallity.fs
@@ -1,8 +1,15 @@
 ﻿module inputlegallity
     
+    open System
+    open System.Collections.Generic
+
     type InputLegallity<'a> =
         //Type to handle illegal input state
         //Intended for use when there is scope for ill-defined or illegal behaviour
         //when typesafe but bad input is used
         |ValidInput of 'a
-        |IllegalInputLength
+        |IllegalInputLength 
+        |IllegalContainerIndex
+
+    exception IllegalInputLengthError
+    exception IllegalContainerIndexError
